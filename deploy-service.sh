3eac756c56cc13a0aaa590ab925f5673a08be3ef
nadazaki21 <nadazaki019@gmail.com>
2023-03-06 19:31:38 +0200

    ..

--- deploy-service.sh
+++ deploy-service.sh
@@ -1,2 +1,2 @@
-aws cloudformation deploy --template-file services.yml   --stack-name "service-${CIRCLE_WORKFLOW_ID:0:5}"  --parameter-overrides  WorkflowID="${CIRCLE_WORKFLOW_ID:0:5}"   --tags project=react   --region=us-east-1
+aws cloudformation deploy --template-file service.yml   --stack-name "service-${CIRCLE_WORKFLOW_ID:0:5}"  --parameter-overrides  WorkflowID="${CIRCLE_WORKFLOW_ID:0:5}"   --tags project=react   --region=us-east-1
       
